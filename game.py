8accdc138acfbef62d139e2d1784c54c84021530
Ruslan Klass <fuwegu50@gmail.com>
2025-03-10 15:17:02 +0700

    Реализация метода get_masked_word для отображения скрытого слова

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
 class Game:
     def __init__(self, secret_word):
-        self.secret_word = secret_word
+        self.secret_word = secret_word.upper()
+        self.guessed_letters = set()
+
+    def get_masked_word(self):
+        return ''.join(letter if letter in self.guessed_letters else '_' for letter in self.secret_word)
